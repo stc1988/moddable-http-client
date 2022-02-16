e959f0610820bef2cfbae6dba27cde11d01fa694
STC <stc1988@users.noreply.github.com>
2022-02-17 00:52:34 +0900

    request header

--- example/main.js
+++ example/main.js
@@ -2,6 +2,7 @@ import client from "client";
 
 const http_get_json = "http://httpbin.org/json"
 const http_get_html = "http://httpbin.org/html"
+const http_get_header = "http://httpbin.org/headers"
 const http_get_200 ="http://httpbin.org/status/200"
 const http_get_400 ="http://httpbin.org/status/400"
 const http_get_image_jpeg = "http://httpbin.org/image/jpeg"
